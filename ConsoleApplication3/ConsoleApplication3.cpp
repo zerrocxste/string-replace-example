675ba2538194b14fca60835915c41e002bcd1f2a
zerrocxste <49279293+zerrocxste@users.noreply.github.com>
2021-02-08 21:07:30 +0600

    add files

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <Windows.h>
+
+void string_replace(char* dest, const char* find, const char* replace)
+{
+	char save_dest[1024] = { NULL };
+	strcpy(save_dest, dest);
+
+	auto dest_legnth = strlen(dest);
+	auto replace_length = strlen(replace);
+	auto find_length = strlen(find);
+
+	for (int i = 0; i < dest_legnth; i++)
+	{
+		auto founded = strstr(dest, find);
+
+		if (founded == NULL)
+			continue;
+
+		if (strcmp(dest + i, founded) != NULL)
+			continue;
+
+		memmove(dest + i, replace, dest_legnth + replace_length);
+		memcpy(dest + strlen(dest), save_dest + i + find_length, dest_legnth + i + find_length);
+	}
+}
+
+int main()
+{
+	char my_str[1024] = "stroki v c kruta!!!!!!!!! 123";
+
+	string_replace(my_str, "kruta!!!!!!!!!", "xuinya...");
+
+	std::cout << my_str << std::endl;
+}
